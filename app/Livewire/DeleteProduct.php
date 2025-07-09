95876eb0eeb3c09322c151693bb3e099133e5c61
rawburger <kyle.bonghanoy.22@usjr.edu.ph>
2025-07-09 13:52:15 +0800

    just components and views

---
+++ app/Livewire/DeleteProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class DeleteProduct extends Component
+{
+    public function render()
+    {
+        return view('livewire.delete-product');
+    }
+}
