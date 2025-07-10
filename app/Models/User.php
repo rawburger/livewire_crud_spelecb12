2cbe696aea17c5931c95305c78a7ee2b6da89a83
rawburger <kyle.bonghanoy.22@usjr.edu.ph>
2025-07-10 14:41:42 +0800

    database added

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,15 +11,14 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
